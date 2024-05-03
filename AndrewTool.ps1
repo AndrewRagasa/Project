84446c4d411183adad9ed19303c2bac3948645e1
AndrewRagasa <168848049+AndrewRagasa@users.noreply.github.com>
2024-05-03 14:49:33 -0500

    Update AndrewTool.ps1

--- AndrewTool.ps1
+++ AndrewTool.ps1
@@ -1,25 +1,25 @@
 Enable-ComputerRestore -Drive "C:\"
 Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
 
-$DebloatFolder = "C:\Temp\AndrewRagasaTool"
-If (Test-Path $DebloatFolder) {
-    Write-Output "$DebloatFolder exists. Skipping."
+$OtimizationFolder = "C:\Temp\AndrewRagasaTool"
+If (Test-Path $OtimizationFolder) {
+    Write-Output "$OtimizationFolder exists. Skipping."
 }
 Else {
-    Write-Output "The folder '$DebloatFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
+    Write-Output "The folder '$OtimizationFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
     Start-Sleep 1
-    New-Item -Path "$DebloatFolder" -ItemType Directory
-    Write-Output "The folder $DebloatFolder was successfully created."
+    New-Item -Path "$OtimizationFolder" -ItemType Directory
+    Write-Output "The folder $OtimizationFolder was successfully created."
 		}
 
-Start-Transcript -OutputDirectory "$DebloatFolder"
+Start-Transcript -OutputDirectory "$OtimizationFolder"
 
 Add-Type -AssemblyName PresentationCore, PresentationFramework
 
 
 		
  #Disables scheduled tasks that are considered unnecessary 
-    Write-Output "Disabling scheduled tasks"
+    Write-Output "Disabled scheduled tasks"
     Get-ScheduledTask  XblGameSaveTaskLogon | Disable-ScheduledTask
     Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
     Get-ScheduledTask  Consolidator | Disable-ScheduledTask
@@ -27,25 +27,25 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     Get-ScheduledTask  DmClient | Disable-ScheduledTask
     Get-ScheduledTask  DmClientOnScenarioDownload | Disable-ScheduledTask
 
-    Write-Output "Stopping and disabling Diagnostics Tracking Service"
+    Write-Output "Stopped and Disabled Diagnostics Tracking Service"
 	
 #Disabling the Diagnostics Tracking Service
     Stop-Service "DiagTrack"
     Set-Service "DiagTrack" -StartupType Disabled
 
     
-    Write-Output "Removing CloudStore from registry if it exists"
+    Write-Output "Removed CloudStore from registry if it exists"
     $CloudStore = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore'
   
  #Disables People icon on Taskbar
-    Write-Output "Disabling People icon on Taskbar"
+    Write-Output "Disabled People icon on Taskbar"
     $People = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People'
     If (Test-Path $People) {
         Set-ItemProperty $People -Name PeopleBand -Value 0
 	}
 
  #Disabling Location Tracking
-    Write-Output "Disabling Location Tracking"
+    Write-Output "Disabled Location Tracking"
     $SensorState = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}"
     $LocationConfig = "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration"
     If (!(Test-Path $SensorState)) {
@@ -58,7 +58,7 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     Set-ItemProperty $LocationConfig Status -Value 0 
 	
 #Turns off Data Collection via the AllowTelemtry key by changing it to 0
-    Write-Output "Turning off Data Collection"
+    Write-Output "Turned off Data Collection"
     $DataCollection1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
     $DataCollection2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
     $DataCollection3 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"    
@@ -73,7 +73,7 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     }
 	
 #Disables Wi-fi Sense
-    Write-Output "Disabling Wi-Fi Sense"
+    Write-Output "Disabled Wi-Fi Sense"
     $WifiSense1 = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting"
     $WifiSense2 = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots"
     $WifiSense3 = "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config"
@@ -88,20 +88,76 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     Set-ItemProperty $WifiSense3  AutoConnectAllowedOEM -Value 0 
 	
  #Stops the Windows Feedback Experience from sending anonymous data
-    Write-Output "Stopping the Windows Feedback Experience program"
+    Write-Output "Stopped the Windows Feedback Experience program"
     $Period = "HKCU:\Software\Microsoft\Siuf\Rules"
    
 	
 #Disables Windows Feedback Experience
-    Write-Output "Disabling Windows Feedback Experience program"
+    Write-Output "Disabled Windows Feedback Experience program"
     $Advertising = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
     If (Test-Path $Advertising) {
         Set-ItemProperty $Advertising Enabled -Value 0 
     }
             
-    #Stops Cortana from being used as part of your Windows Search Function
-    Write-Output "Stopping Cortana from being used as part of your Windows Search Function"
+#Stops Cortana from being used as part of your Windows Search Function
+    Write-Output "Stopped Cortana from being used as part of your Windows Search Function"
     $Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
     If (Test-Path $Search) {
         Set-ItemProperty $Search AllowCortana -Value 0 
     }
+
+#Disabling the Diagnostics Tracking Service
+    Stop-Service "DiagTrack"
+    Set-Service "DiagTrack" -StartupType Disabled
+	
+$Keys = @(
+            
+#Remove Background Tasks
+        "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+		
+	 )
+        
+    #This writes the output of each key it is removing and also removes the keys listed above.
+    ForEach ($Key in $Keys) {
+        Write-Output "Removed $Key from registry"
+        Remove-Item $Key -Recurse
+    }
+}
+Write-Output "Removed Weather App"
+        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingWeather" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
+        #Money App
+        Write-Output "Removed Money App"
+        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingFinance" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
+        #Sports App
+        Write-Output "Removed Sports App"
+        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingSports" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
+		#XBOX App
+        Write-Output "Removed XBOX App"
+        Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Like"Microsoft.XboxApp"} | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName}
+        #Sway App
+        Write-Output "Removed Sway App"
+        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.Office.Sway" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
+		
+		
+ #Disable Unnecessary Windows Services
+        Stop-Service "MessagingService"
+        Set-Service "MessagingService" -StartupType Disabled
+        Stop-Service "PimIndexMaintenanceSvc"
+        Set-Service "PimIndexMaintenanceSvc" -StartupType Disabled
+        Stop-Service "RetailDemo"
+        Set-Service "RetailDemo" -StartupType Disabled
+        Stop-Service "MapsBroker"
+        Set-Service "MapsBroker" -StartupType Disabled
+        Stop-Service "UnistoreSvc"
+        Set-Service "UnistoreSvc" -StartupType Disabled
+		
+		
+chkdsk
+
+sfc /scannow
+
+DISM /Online /Cleanup-Image /CheckHealth
+
+DISM /Online /Cleanup-Image /ScanHealth
+
+DISM /Online /Cleanup-Image /RestoreHealth
