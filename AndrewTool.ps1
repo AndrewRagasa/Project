72c33b23b86e6936b6cd0e5066fd884154f45cfb
AndrewRagasa <168848049+AndrewRagasa@users.noreply.github.com>
2024-05-06 10:36:33 -0500

    Update AndrewTool.ps1

--- AndrewTool.ps1
+++ AndrewTool.ps1
@@ -2,22 +2,12 @@ Enable-ComputerRestore -Drive "C:\"
 Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
 
 Write-Output "DrewOptimization V1.2 ** Restore Point Created"
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 
 $OtimizationFolder = "C:\Temp\AndrewRagasaTool"
 If (Test-Path $OtimizationFolder) {
     Write-Output "$OtimizationFolder exists. Skipping."
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
+
 	
 }
 Else {
@@ -25,10 +15,7 @@ Else {
     Start-Sleep 1
     New-Item -Path "$OtimizationFolder" -ItemType Directory
     Write-Output "DrewOptimization V1.2 ** The folder $OtimizationFolder was successfully created."
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
+
 		}
 
 Start-Transcript -OutputDirectory "$OtimizationFolder"
@@ -40,24 +27,18 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     
     Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
 	Write-Output "DrewOptimization V1.2 ** Disabled XblGameSaveTask"
-	Write-Output "**"
-	Write-Output "**"
     Get-ScheduledTask  Consolidator | Disable-ScheduledTask
 	Write-Output "DrewOptimization V1.2 ** Disabled ConsolidatorTask"
-	Write-Output "**"
-	Write-Output "**"
+	
     Get-ScheduledTask  UsbCeip | Disable-ScheduledTask
 	Write-Output "DrewOptimization V1.2 ** Disabled UsbCeipTask"
-	Write-Output "**"
-	Write-Output "**"
+	
     Get-ScheduledTask  DmClient | Disable-ScheduledTask
 	Write-Output "DrewOptimization V1.2 ** Disabled DmClientTask"
-	Write-Output "**"
-	Write-Output "**"
+	W
     Get-ScheduledTask  DmClientOnScenarioDownload | Disable-ScheduledTask
 	Write-Output "DrewOptimization V1.2 ** Disabled DmClientOnScenarioDownloadTask"
-	Write-Output "**"
-	Write-Output "**"
+	
 
     
 	
@@ -65,15 +46,11 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     Stop-Service "DiagTrack"
     Set-Service "DiagTrack" -StartupType Disabled
 	Write-Output "DrewOptimization V1.2 ** Stopped and Disabled Diagnostics Tracking Service"
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
+	
     
     Write-Output "DrewOptimization V1.2 ** Removed CloudStore from registry if it exists"
     $CloudStore = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore'
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
+	
   
  #Disables People icon on Taskbar
     Write-Output "DrewOptimization V1.2 ** Disabled People icon on Taskbar"
@@ -81,9 +58,7 @@ Add-Type -AssemblyName PresentationCore, PresentationFramework
     If (Test-Path $People) {
         Set-ItemProperty $People -Name PeopleBand -Value 0
 	}	
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 #Disabling Location Tracking
     Write-Output "DrewOptimization V1.2 **  Disabled Location Tracking"
     $SensorState = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}"
@@ -96,9 +71,7 @@ Write-Output "**"
         New-Item $LocationConfig
     }
     Set-ItemProperty $LocationConfig Status -Value 0 
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 #Turns off Data Collection via the AllowTelemtry key by changing it to 0
     Write-Output "DrewOptimization V1.2 ** Turned off Data Collection"
     $DataCollection1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
@@ -113,9 +86,7 @@ Write-Output "**"
     If (Test-Path $DataCollection3) {
         Set-ItemProperty $DataCollection3  AllowTelemetry -Value 0 
     }
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 #Disables Wi-fi Sense
     Write-Output "DrewOptimization V1.2 ** Disabled Wi-Fi Sense"
     $WifiSense1 = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting"
@@ -130,26 +101,19 @@ Write-Output "**"
     }
     Set-ItemProperty $WifiSense2  Value -Value 0 
     Set-ItemProperty $WifiSense3  AutoConnectAllowedOEM -Value 0 
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 	
  #Stops the Windows Feedback Experience from sending anonymous data
     Write-Output "DrewOptimization V1.2 ** Stopped the Windows Feedback Experience program"
     $Period = "HKCU:\Software\Microsoft\Siuf\Rules"
-   
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 #Disables Windows Feedback Experience
     Write-Output "DrewOptimization V1.2 ** Disabled Windows Feedback Experience program"
     $Advertising = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
     If (Test-Path $Advertising) {
         Set-ItemProperty $Advertising Enabled -Value 0 
     }
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
+
 #Stops Cortana from being used as part of your Windows Search Function
     Write-Output "DrewOptimization V1.2 ** Stopped Cortana from being used as part of your Windows Search Function"
     $Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
@@ -157,69 +121,53 @@ Write-Output "**"
         Set-ItemProperty $Search AllowCortana -Value 0 
     }
 
-Write-Output "**"
-Write-Output "**"
-Write-Output "**"
         
 
 Write-Output "DrewOptimization V1.2 ** Removed Weather App"
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingWeather" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
         #Money App
-		Write-Output "**"
-		Write-Output "**"
+		
         Write-Output "DrewOptimization V1.2 ** Removed Money App"
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingFinance" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
         #Sports App
-		Write-Output "**"
-		Write-Output "**"
+		
         Write-Output "DrewOptimization V1.2 ** Removed Sports App"
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.BingSports" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
 		#XBOX App
-		Write-Output "**"
-		Write-Output "**"
+		
         Write-Output "DrewOptimization V1.2 ** Removed XBOX App"
         Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Like"Microsoft.XboxApp"} | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName}
         #Sway App
-		Write-Output "**"
-		Write-Output "**"
+		
         Write-Output "DrewOptimization V1.2 ** Removed Sway App"
         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.Office.Sway" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
 		
 		
- #Disable Unnecessary Windows Services
-        Stop-Service "MessagingService"
+         Stop-Service "MessagingService"
         Set-Service "MessagingService" -StartupType Disabled
 		Write-Output "DrewOptimization V1.2 ** Disabled MessagingService"
-		Write-Output "**"
+		
         Stop-Service "PimIndexMaintenanceSvc"
         Set-Service "PimIndexMaintenanceSvc" -StartupType Disabled
 		Write-Output "DrewOptimization V1.2 ** Disabled PimIndexMaintenanceSvc"
-		Write-Output "**"
+		
         Stop-Service "RetailDemo"
         Set-Service "RetailDemo" -StartupType Disabled
 		Write-Output "DrewOptimization V1.2 ** Disabled RetailDemo"
-		Write-Output "**"
+		
         Stop-Service "MapsBroker"
         Set-Service "MapsBroker" -StartupType Disabled
 		Write-Output "DrewOptimization V1.2 ** Disabled MapsBroker"
-		Write-Output "**"
+		
         Stop-Service "UnistoreSvc"
         Set-Service "UnistoreSvc" -StartupType Disabled
 		Write-Output "DrewOptimization V1.2 ** Disabled UnistoreSvc"
-		Write-Output "**"
+		
 #Disabling the Diagnostics Tracking Service
     Stop-Service "DiagTrack"
     Set-Service "DiagTrack" -StartupType Disabled
-
-
-	Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
 	Write-Output "DrewOptimization V1.2 ** Disabled DiagTrack"		
 
-Write-Output "**"
-	Write-Output "**"
-	Write-Output "**"
 
 Write-Output "DrewOptimization V1.2 ** Checking Disk"
 
