6ec844d3fcc8f22b5de7ebc3c880bebb4d7486a9
AndrewRagasa <168848049+AndrewRagasa@users.noreply.github.com>
2025-03-28 16:05:49 -0500

    Update AndrewTool.ps1

--- AndrewTool.ps1
+++ AndrewTool.ps1
@@ -185,6 +185,6 @@ Write-Output "DrewOptimization V1.2 ** Scanning Corrupted Files and Images"
 DISM /Online /Cleanup-Image /ScanHealth
 
 
-Read-Host -Prompt "Optimization Complete - You can close the window"
+Read-Host -Prompt "AndrewRagasa Optimization Tool™ Complete - You can close the window "
   
 
